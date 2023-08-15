dc7e7856ded860b3f225ab8bfc38e185709d276b
Simon Willans <simon@engageinteractive.co.uk>
2023-08-15 11:15:15 +0100

    Adds some base packages (Tailwind, Vue)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
+import tailwind from '@astrojs/tailwind';
 
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
   server: {
     host: '0.0.0.0',
   },
+  integrations: [vue(), tailwind({ applyBaseStyles: false })],
 });
